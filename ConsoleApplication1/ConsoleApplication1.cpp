2d285a44be97e94bd13fadae58e89f49829ca00b
Max i <a1612180169@outlook.com>
2022-01-18 20:19:28 +0800

    init

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,154 @@
+﻿#include <iostream>
+#include <iomanip> 
+//#include <string>
+#include<stdio.h>
+#include<math.h>
+//#include <cstdio>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+}
+
+/*int main() {
+	double f;
+	cin >> f;
+	cout << fixed << setprecision(5) << 5 * (f - 32) / 9 << endl;
+	return 0;
+}*/
+
+/* int main() {
+		double x, a, b, c, d;
+		scanf("%lf%lf%lf%lf%lf", &x, &a, &b, &c, &d);
+		double result = a * pow(x, 3) + b * pow(x, 2) + c * x + d;
+		printf("%.7lf", result);
+		return 0;
+
+}*/
+
+/*
+int main() {
+	double a, b;
+	cin >> a;
+	cin >> b;
+	cout << fixed << setprecision(3) << b / a *100<<"%";
+}
+*/
+
+/*int main() {
+	long long int a, b;
+	long long int c;
+	scanf("%lld%lld", &a, &b);
+	c = a * b;
+	printf("%lld\n", c);
+	return 0;
+}*/
+
+/*int main() {
+	double a, b;
+	cin >> a;
+	cin >> b;
+	cout << fixed << setprecision(9)<< a / b<< endl;
+}*/
+
+/*int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << a / b << " ";
+	cout << a % b;
+}*/
+
+/*int main() {
+	int a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << (a + b) * c;
+}*/
+
+/*int main() {
+	int a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << (a + b) / c;
+}*/
+
+/*size_t getPtrSize(char* ptr)
+{
+	return sizeof(ptr);
+}*/
+
+/*
+int main()
+{
+	int num1 , num2;
+	scanf_s("%d%d",&num1,&num2);
+	printf("%d", num1+num2);
+	std::cout << "Hello World!\n";
+	printf(sizeof int);
+	printf(sizeof char);
+	printf(sizeof double);
+
+	return 0;
+
+}
+*/
+
+/*
+int main()
+{
+	// sizeof()
+	cout << "The size of a char is: "
+		<< sizeof(char)
+		<< "\nThe size of a double is: "
+		<< sizeof(double)
+		<< "\nThe size of a int is: "
+		<< sizeof(int);
+}
+*/
+
+/*int main() {
+  int a = 'c' + 2;
+
+  printf("%c", a);
+  return 0;
+}*/
+
+/*int main() {
+  int n, m;
+  cin >> n;
+  m = 2;
+  if (n < 1) {
+	  m = 1;
+  };
+  for (size_t i = 1; i < n; i++) {
+	  m = m * 2;
+  }
+  cout << m;
+}*/
+
+/* int main() {
+   int n, m, l;
+   cin >> n;
+   cin >> m;
+   cin >> l;
+   cout << m;
+}*/
+
+/*int main() {
+  int a, b;
+  cin >> a;
+  cin >> b;
+  cout << a + b;
+}*/
+
+/*int main() {
+   char a;
+   cin >> a;
+   cout << "  " << a;
+   cout << "\n " << a << a << a;
+   cout << "\n" << a << a << a << a << a;
+}*/
